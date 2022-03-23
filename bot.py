668487e503b497fbe184860cb833b7302a79c2b7
affan <47723802+afnzmn@users.noreply.github.com>
2022-03-23 01:12:55 -0400

    added comments

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import tweepy, ssl, pandas, csv, re, random
 from wordle import *
 
-guess_list = open('avawords.txt').read().splitlines()
+guess_list = open('available_words.txt').read().splitlines()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -14,18 +14,22 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+#get the most recent tweet's id from the bot's timeline
 name = 'WordleGame_Bot'
 tweets_list= api.user_timeline(screen_name=name, count=1, exclude_replies=True)
 recent_tweet = tweets_list[0]
 tweet_id = recent_tweet.id_str
 
 replies=[]
-for tweet in tweepy.Cursor(api.search_tweets,q='to:'+name, result_type='popularity').items(250):
+#get 300 tweets of the most recent tweets replying to the bot
+for tweet in tweepy.Cursor(api.search_tweets,q='to:'+name, result_type='recent').items(1000):
+    #if the replies are replying to the most recent tweet, add tweet to replies list
     if hasattr(tweet, 'in_reply_to_status_id_str'):
         if (tweet.in_reply_to_status_id_str==tweet_id):
             replies.append(tweet)
 
 with open('replies.csv', 'w') as f:
+    #create csv of id, text, and likes of each tweet in replies
     csv_writer = csv.DictWriter(f, fieldnames=('ID', 'TEXT', 'LIKES'))
     csv_writer.writeheader()
     for tweet in replies:
@@ -33,29 +37,36 @@ with open('replies.csv', 'w') as f:
         text = text.replace('\n', ' ')
         text = text.lower()
         row = {'ID': tweet.id_str, 'TEXT': text, 'LIKES': tweet.favorite_count}
-        if '\[' and ']' in text or len(text) == 5: #i see you @ThatOneCalculator this ones for you
+        if '\[' and ']' in text or len(text) == 5:
             csv_writer.writerow(row)
-    
+  
 df = pandas.read_csv('replies.csv')
 df = df.sort_values('LIKES', ascending=False)
 df.to_csv('replies.csv', index=False)  
 
 df2 = pandas.read_csv('counter.csv')
 
-if df2.at[0,'CURRENT_ROW'] == 7 or df2.at[0,'CURRENT_ROW'] == 0 or not bool(df2.at[0,'NOT_WIN']):
+if df2.at[0,'CURRENT_ROW'] == 7 or df2.at[0,'CURRENT_ROW'] == 0 or bool(df2.at[0,'WIN']):
     api.update_status(str(start()))
 
 else:
+    #get the text within the FIRST set of square brackets of the tweet (this wont cause any errors if the tweet has no brackets)
     for i,content in enumerate(df['TEXT']):
         bracketless = re.search(r"\[([A-Za-z0-9_]+)\]", content)
         if bracketless:
             df.at[i, 'TEXT'] = bracketless.group(1)
     df.to_csv('replies.csv', index=False)  
 
-    guess = df.iat[i,1]
-    api.update_status(status = 'thanks', in_reply_to_status_id = df.iat[i,0] , auto_populate_reply_metadata=True)
+    for i,content in enumerate(df['TEXT']):
+        if content in guess_list and len(content) == 5:
+            guess = df.iat[i,1]
+            break
 
-    if 'guess' not in globals():
+    #if there are no valid replies (lel dead bot), choose a random word from the guess list and use that
+    if 'guess' not in locals() and 'guess' not in globals():
         guess = random.choice(guess_list)
 
-    api.update_status(str(main(guess, current_row, not_win, new, correct_word, word_num)))
+    else:
+        api.update_status(status = 'thanks', in_reply_to_status_id = df.iat[i,0] , auto_populate_reply_metadata=True)
+
+    api.update_status(str(main(guess, current_row, win, wordle_grid, correct_word, word_num)))
